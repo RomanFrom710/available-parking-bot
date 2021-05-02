b302f2e7fbc2f61260ab866d01c6a24d54cafd15
Roman Chaley <romanchaley@gmail.com>
2021-05-02 23:27:17 +0300

    Restrict the bot to allowed ids only

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
+const allowedIdsList = process.env.TELEGRAM_BOT_ALLOWED_IDS;
+if (!token || !allowedIdsList) {
+  throw new Error('Required env variables cannot be found');
+}
+
+const allowedIds = allowedIdsList.split(',');
 
 const bot = new TelegramBot(token, {polling: true});
 
-bot.on('message', (msg) => {
+bot.onText(/\/getpic/, (msg) => {
+  if (!allowedIds.includes(msg.from.id.toString())) {
+    bot.sendMessage(msg.chat.id, 'You are not allowed!');
+    return;
+  }
+
   bot.sendMessage(msg.chat.id, 'hi!');
 });
